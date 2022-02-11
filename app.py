755dad29a75901e17b6510b30db468a616fdcdd9
grklakg <b.rtm.mlkn@gmail.com>
2022-02-11 19:04:44 +0400

    Added db & first pages

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@localhost/users?client_encoding=utf8'
 
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<users {self.id}>"
+
+class Profiles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(50), unique=True, nullable=False)
+    old = db.Column(db.Integer)
+    city = db.Column(db.String(100))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f"<profiles {self.id}>"
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!123'
+def index():
+    return render_template('index.html', title="Demo project by Artem")
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = Users(email, password)
+        db.session.add(user)
+        db.session.commit()
+        return render_template('signup.html', title="УСПЕШНО!")
+    return render_template('signup.html', title="Demo project by Artem")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
